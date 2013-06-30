7fa6a39f3ef33ec311f5af630add9f559ab90db7
Andreas Nauleau <andreas@adap.tv>
2013-06-30 10:23:46 -0700

    first commit but big

--- client/pride.js
+++ client/pride.js
@@ -1,3 +1,7 @@
+var INSTAID = 'c28bc8730c734259aba3fd5c1946f073';
+var markersArray = [];
+var instaArray = [];
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to pride.";
@@ -7,6 +11,7 @@ if (Meteor.isClient) {
     'click input' : function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
+        getNewPhotos();
         console.log("You pressed the button");
     }
   });
@@ -14,6 +19,25 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+  });
+}
+
+function getNewPhotos () {
+$.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url: "https://api.instagram.com/v1/tags/pride/media/recent/?client_id=c28bc8730c734259aba3fd5c1946f073",
+        success: function(data) {
+          render(data);
+}});
+}
+
+function render (data) {
+  var data = data.data;
+  $.each(data, function (ind, val) {
+    var photoSRC = val.images.low_resolution.url;
+    console.log(photoSRC);
+    $('#photoBoard').append("<img class='pridePic' src='" + photoSRC + "'></img>");
   });
 }
